287729c61136eade081eabf1ace4bb740b080b93
Soplakov <soplakov@gmail.com>
2022-05-03 19:32:36 +0500

    Добавил метод запроса друзей пользователя из сети

--- VkontakteApp028/Network/NetworkManager.swift
+++ VkontakteApp028/Network/NetworkManager.swift
@@ -8,4 +8,31 @@
 import Foundation
 
 // Запросы к Vkontakte API
-
+class NetworkManager {
+    
+    // Метод для запроса друзей пользователя
+    static func getFriends() {
+        var urlConstructor = URLComponents()
+        urlConstructor.scheme = "https"
+        urlConstructor.host = "api.vk.com"
+        urlConstructor.path = "/method/friends.get"
+        urlConstructor.queryItems = [
+            URLQueryItem(name: "access_token", value: MySession.instance.token),
+            URLQueryItem(name: "v", value: "5.131"),
+            URLQueryItem(name: "count", value: "5"),
+            URLQueryItem(name: "fields", value: "nickname, photo_100")
+        ]
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        
+        let task = session.dataTask(with: urlConstructor.url!) { data, response, error in
+            do {
+                let friends = try JSONDecoder().decode(FriendsResponseVK.self, from: data!)
+                
+            } catch (let error) {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
