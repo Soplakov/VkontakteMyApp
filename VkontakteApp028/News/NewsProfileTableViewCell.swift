e81c4f9e759f291ae587f3448e4742cd83718e20
Soplakov <soplakov@gmail.com>
2022-06-20 18:14:16 +0500

    Добавил IBOutlet из storyboard на контроллеры с ячейками Новостей text, photo, data

---
+++ VkontakteApp028/News/NewsProfileTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewsProfileTableViewCell.swift
+//  VkontakteApp028
+//
+//  Created by PiZero on 14.06.2022.
+//
+
+import UIKit
+
+class NewsProfileTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var newsAvatar: UIImageView!
+    
+    @IBOutlet weak var newsName: UILabel!
+    
+}
+
---
+++ VkontakteApp028/News/StatisticTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  StatisticTableViewCell.swift
+//  VkontakteApp028
+//
+//  Created by PiZero on 14.06.2022.
+//
+
+import UIKit
+
+class StatisticTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var newsLikeCount: UILabel!
+    
+    @IBOutlet weak var newsCommentCount: UILabel!
+    
+    @IBOutlet weak var newsRepostCount: UILabel!
+    
+    @IBOutlet weak var newsViewCount: UILabel!
+    
+}
+
+
